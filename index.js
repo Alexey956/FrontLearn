02049c5e283a4d4222505deea10e8aa03a28623e
Alexey956 <alex1996bez@gmail.com>
2024-10-10 23:46:36 +0300

    First commit

---
+++ index.js
@@ -0,0 +1,15 @@
+console.log(1);
+
+const button = document.querySelector('button');
+const output = document.querySelector('.output');
+
+button.addEventListener('mouseover', function() {
+    output.innerHTML = 'Мама, я тебя люблю!';
+    output.style.opacity = '1';
+})
+
+button.addEventListener('mouseout', function() {
+    output.innerHTML = '';
+    
+})
+
