03a239060ab8fe168a90df1157e95f0dfffb7cf0
Alexey956 <alex1996bez@gmail.com>
2024-10-11 09:55:04 +0300

    Изменил надпись

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ const button = document.querySelector('button');
 const output = document.querySelector('.output');
 
 button.addEventListener('mouseover', function() {
-    output.innerHTML = 'Мама, я тебя люблю!';
+    output.innerHTML = 'Лиза, я тебя люблю!';
     output.style.opacity = '1';
 })
 
 button.addEventListener('mouseout', function() {
     output.innerHTML = '';
-    
+
 })
 
